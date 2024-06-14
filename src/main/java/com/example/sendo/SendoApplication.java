d8377d4ac625a975a0be5822255b7f71c77d1e98
taing28 <taing280703@gmail.com>
2024-06-14 14:43:44 +0700

    first commit

---
+++ src/main/java/com/example/sendo/SendoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sendo/SendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
