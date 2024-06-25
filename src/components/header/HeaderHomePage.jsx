f9232c3a5a682a37f59c8645d1a9aa19b110ba57
taing28 <taing280703@gmail.com>
2024-06-25 14:39:44 +0700

    Hover to icon and search at header

--- src/components/header/HeaderHomePage.jsx
+++ src/components/header/HeaderHomePage.jsx
@@ -1,13 +1,25 @@
-import { Button, Col, Form, Input, Row } from "antd";
+import { Button, Col, Form, Input, Popover, Row } from "antd";
 import { memo } from "react";
 import logo from '../../assets/image/sendo-logo.png'
 import '../../assets/css/style.css'
 import { Container } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBorderAll } from '@fortawesome/free-solid-svg-icons'
-import { SearchOutlined } from '@ant-design/icons'
+import { SearchOutlined, ShoppingOutlined, AppstoreAddOutlined } from '@ant-design/icons'
 
 export const HeaderHomePage = memo(() => {
+    const contentCategory = (
+        <div>
+            <p>Content</p>
+            <p>Content</p>
+        </div>
+    );
+
+    const contentSearch = (
+        <div>
+            <p>item 1</p>
+            <p>item 1</p>
+        </div>
+    )
+
     return (
         <Container>
             <Row>
@@ -18,22 +30,40 @@ export const HeaderHomePage = memo(() => {
                 </Col>
                 <Col span={18}>
                     <Row>
-                        <Col span={2}>
-                            <FontAwesomeIcon className="icon-header" icon={faBorderAll} />
+                        <Col span={2} style={{ lineHeight: '64px' }}>
+                            <Popover content={contentCategory} title="Tieu de" trigger="hover">
+                                <AppstoreAddOutlined className="icon-header" />
+                            </Popover>
                         </Col>
                         <Col span={22}>
                             <Form>
-                                <div className="d-flex align-items-center justify-center">
-                                        <Input placeholder="Search" />
-                                        <Button>
+                                <Row>
+                                    <Col span={22} style={{ lineHeight: '64px' }}>
+                                        <Popover content={contentSearch} title="Title" trigger="focus">
+                                            <Input placeholder="Search" />
+                                        </Popover>
+                                    </Col>
+                                    <Col span={2} style={{ lineHeight: '64px' }}>
+                                        <Button style={{ transform: 'translateY(-5%)' }}>
                                             <SearchOutlined />
                                         </Button>
-                                </div>
+                                    </Col>
+                                </Row>
+
                             </Form>
                         </Col>
                     </Row>
                 </Col>
-                <Col span={4}>login</Col>
+                <Col span={4}>
+                    <Row>
+                        <Col span={12}>
+                            <ShoppingOutlined className="icon-header" />
+                        </Col>
+                        <Col span={12}>
+                            <Button style={{ fontWeight: 'bold' }}>Đăng nhập</Button>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     )
